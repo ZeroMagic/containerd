584f5bc0a5126036abe36d504ecc346452dfb43e
ZeroMagic <anthonyliu@zju.edu.cn>
2018-07-11 16:38:21 +0800

    log
    Signed-off-by: ZeroMagic <anthonyliu@zju.edu.cn>

--- runtime/kata/task.go
+++ runtime/kata/task.go
@@ -91,6 +91,7 @@ func (t *Task) Start(ctx context.Context) error {
 
 // State returns runtime information for the task
 func (t *Task) State(ctx context.Context) (runtime.State, error) {
+	logrus.FieldLogger(logrus.New()).Infof("kata task %v State", t.id)
 	p := t.processList[t.id]
 
 	state, err := p.Status(ctx)
