906e419bdb06cd96d2f63a1c5645ca1c6045b8d9
ZeroMagic <anthonyliu@zju.edu.cn>
2018-07-21 18:41:37 +0800

    sandbox and container config
    Signed-off-by: ZeroMagic <anthonyliu@zju.edu.cn>

--- vendor/github.com/kata-containers/runtime/virtcontainers/sandbox.go
+++ vendor/github.com/kata-containers/runtime/virtcontainers/sandbox.go
@@ -673,6 +673,10 @@ func createSandbox(sandboxConfig SandboxConfig) (*Sandbox, error) {
 		return nil, err
 	}
 
+	logrus.FieldLogger(logrus.New()).WithFields(logrus.Fields{
+		"sandboxConfig":  	sandboxConfig,
+	}).Info("##### sandbox createSandbox #####")
+
 	s, err := newSandbox(sandboxConfig)
 	if err != nil {
 		return nil, err
@@ -969,6 +973,9 @@ func (s *Sandbox) newContainers() error {
 
 // CreateContainer creates a new container in the sandbox
 func (s *Sandbox) CreateContainer(contConfig ContainerConfig) (VCContainer, error) {
+	logrus.FieldLogger(logrus.New()).WithFields(logrus.Fields{
+		"contConfig":  	contConfig,
+	}).Info("##### sandbox CreateContainer #####")
 	// Create the container.
 	logrus.FieldLogger(logrus.New()).Info("##### container sandbox create start #####")
 	c, err := createContainer(s, contConfig)
